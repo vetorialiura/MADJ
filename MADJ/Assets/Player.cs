e897d0b60f0d082d6111e446bca11d249c8c5d38
vetorialiura <146194890+vetorialiura@users.noreply.github.com>
2025-02-21 11:17:14 -0300

    quase a merma coisa so que com moon walk

--- MADJ/Assets/Player.cs
+++ MADJ/Assets/Player.cs
@@ -7,11 +7,11 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
-    private Rigidbody2D rig;    
+    private Rigidbody2D rig;
     public bool isJumping;
     public bool doublejump;
     private Animator anim;
-   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,67 +23,31 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
-        Jump();
+       // Jump();
     }
 
     void Move()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speed;
-       
+
         if (Input.GetAxis("Horizontal") > 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
+
         if (Input.GetAxis("Horizontal") < 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
 
         }
+
         if (Input.GetAxis("Horizontal") == 0f)
         {
             anim.SetBool("walk", false);
         }
     }
-
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (!isJumping)
-            {
-                rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                doublejump = true;
-                anim.SetBool("Jump", true);
-            }
-            else
-            {
-                if (doublejump)
-                {
-                    rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                    doublejump = false;
-                }
-            }
-           
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
-        {
-            isJumping = false;
-            anim.SetBool("Jump", false);
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
-        {
-            isJumping = true;
-        }
-    }
 }
+    
